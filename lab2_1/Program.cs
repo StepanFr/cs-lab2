565f4cf5470fe4746220d90a473ed71112e2be55
StepanFr <stepanfr@yandex.ru>
2024-11-11 12:21:24 +0300

    lab2

---
+++ lab2_1/Program.cs
@@ -0,0 +1,148 @@
+﻿namespace lab2_2
+{
+
+    using System;
+
+    namespace ClassroomExample
+    {
+        public class Pupil
+        {
+            public virtual void Study()
+            {
+                Console.WriteLine("Pupil is studying.");
+            }
+
+            public virtual void Read()
+            {
+                Console.WriteLine("Pupil is reading.");
+            }
+
+            public virtual void Write()
+            {
+                Console.WriteLine("Pupil is writing.");
+            }
+
+            public virtual void Relax()
+            {
+                Console.WriteLine("Pupil is relaxing.");
+            }
+        }
+
+        public class ExcelentPupil : Pupil
+        {
+            public override void Study()
+            {
+                Console.WriteLine("ExcelentPupil is studying excellently.");
+            }
+
+            public override void Read()
+            {
+                Console.WriteLine("ExcelentPupil is reading excellently.");
+            }
+
+            public override void Write()
+            {
+                Console.WriteLine("ExcelentPupil is writing excellently.");
+            }
+
+            public override void Relax()
+            {
+                Console.WriteLine("ExcelentPupil is relaxing excellently.");
+            }
+        }
+
+        public class GoodPupil : Pupil
+        {
+            public override void Study()
+            {
+                Console.WriteLine("GoodPupil is studying well.");
+            }
+
+            public override void Read()
+            {
+                Console.WriteLine("GoodPupil is reading well.");
+            }
+
+            public override void Write()
+            {
+                Console.WriteLine("GoodPupil is writing well.");
+            }
+
+            public override void Relax()
+            {
+                Console.WriteLine("GoodPupil is relaxing well.");
+            }
+        }
+
+        public class BadPupil : Pupil
+        {
+            public override void Study()
+            {
+                Console.WriteLine("BadPupil is not studying well.");
+            }
+
+            public override void Read()
+            {
+                Console.WriteLine("BadPupil is not reading well.");
+            }
+
+            public override void Write()
+            {
+                Console.WriteLine("BadPupil is not writing well.");
+            }
+
+            public override void Relax()
+            {
+                Console.WriteLine("BadPupil is not relaxing well.");
+            }
+        }
+
+        public class ClassRoom
+        {
+            private Pupil[] pupils;
+
+            public ClassRoom(params Pupil[] pupils)
+            {
+                if (pupils.Length < 2 || pupils.Length > 4)
+                {
+                    throw new ArgumentException("The number of pupils must be between 2 and 4.");
+                }
+
+                this.pupils = pupils;
+            }
+
+            public void ShowPupilsSkills()
+            {
+                foreach (var pupil in pupils)
+                {
+                    Console.WriteLine("Pupil:");
+                    pupil.Study();
+                    pupil.Read();
+                    pupil.Write();
+                    pupil.Relax();
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //экземпляры учеников
+                Pupil pupil1 = new ExcelentPupil();
+                Pupil pupil2 = new GoodPupil();
+                Pupil pupil3 = new BadPupil();
+                Pupil pupil4 = new ExcelentPupil();
+
+                //экземпляр класса ClassRoom
+                ClassRoom classRoom = new(pupil1, pupil2, pupil3, pupil4);
+
+                //выводим информацию о навыках учеников
+                classRoom.ShowPupilsSkills();
+            }
+        }
+    }
+
+
+}
---
+++ lab2_2/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace lab2_2
+{
+
+    using System;
+
+    namespace VehicleExample
+    {
+        //базовый класс Vehicle
+        public class Vehicle
+        {
+            public (double X, double Y) Coordinates { get; set; }
+            public decimal Price { get; set; }
+            public double Speed { get; set; }
+            public int YearOfManufacture { get; set; }
+
+            //конструктор
+            public Vehicle((double X, double Y) coordinates, decimal price, double speed, int yearOfManufacture)
+            {
+                Coordinates = coordinates;
+                Price = price;
+                Speed = speed;
+                YearOfManufacture = yearOfManufacture;
+            }
+
+            //метод для вывода информации о транспортном средстве
+            public virtual void ShowInfo()
+            {
+                Console.WriteLine($"Coordinates: ({Coordinates.X}, {Coordinates.Y})");
+                Console.WriteLine($"Price: {Price} $");
+                Console.WriteLine($"Speed: {Speed} km/h");
+                Console.WriteLine($"Year of Manufacture: {YearOfManufacture}");
+            }
+        }
+
+        public class Plane : Vehicle
+        {
+            public double Height { get; set; }
+            public int NumberOfPassengers { get; set; }
+
+            public Plane((double X, double Y) coordinates, decimal price, double speed, int yearOfManufacture, double height, int numberOfPassengers)
+                : base(coordinates, price, speed, yearOfManufacture)
+            {
+                Height = height;
+                NumberOfPassengers = numberOfPassengers;
+            }
+
+            //переопределение метода ShowInfo
+            public override void ShowInfo()
+            {
+                base.ShowInfo();
+                Console.WriteLine($"Height: {Height} meters");
+                Console.WriteLine($"Number of Passengers: {NumberOfPassengers}");
+            }
+        }
+
+        public class Car : Vehicle
+        {
+            public Car((double X, double Y) coordinates, decimal price, double speed, int yearOfManufacture)
+                : base(coordinates, price, speed, yearOfManufacture)
+            {
+            }
+
+            //переопределение метода ShowInfo
+            public override void ShowInfo()
+            {
+                base.ShowInfo();
+            }
+        }
+
+        public class Ship : Vehicle
+        {
+            public int NumberOfPassengers { get; set; }
+            public string PortOfRegistry { get; set; }
+
+            public Ship((double X, double Y) coordinates, decimal price, double speed, int yearOfManufacture, int numberOfPassengers, string portOfRegistry)
+                : base(coordinates, price, speed, yearOfManufacture)
+            {
+                NumberOfPassengers = numberOfPassengers;
+                PortOfRegistry = portOfRegistry;
+            }
+
+            //переопределение метода ShowInfo
+            public override void ShowInfo()
+            {
+                base.ShowInfo();
+                Console.WriteLine($"Number of Passengers: {NumberOfPassengers}");
+                Console.WriteLine($"Port of Registry: {PortOfRegistry}");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //экземпляры транспортных средств
+                Vehicle plane = new Plane((34.05, -118.25), 2000000m, 900, 2020, 12000, 150);
+                Vehicle car = new Car((40.71, -74.01), 30000m, 150, 2023);
+                Vehicle ship = new Ship((51.51, -0.12), 500000m, 30, 2015, 500, "London");
+
+                //выводим информацию о транспортных средствах
+                Console.WriteLine("Plane Information:");
+                plane.ShowInfo();
+                Console.WriteLine();
+
+                Console.WriteLine("Car Information:");
+                car.ShowInfo();
+                Console.WriteLine();
+
+                Console.WriteLine("Ship Information:");
+                ship.ShowInfo();
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+}
